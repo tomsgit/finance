84b6bfcb5c54ddb74c79880c265c1c1d87b9979a
toms.varghese <tomsvarghese@gmail.com>
2018-05-01 16:52:31 +0530

    shell of env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  },
+  quandl:{
+    apiKey:""
+  }
 };
